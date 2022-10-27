fcb3a25d95d634eb04bb08e1c820d6782f3aa563
Nina Zhukava <a01480b397@gmail.com>
2022-10-27 10:09:01 +0300

    task 14 sandbox

---
+++ src/main/java/homework/day8/SandBox.java
@@ -0,0 +1,43 @@
+package src.main.java.homework.day8;
+
+import src.main.java.homework.day8.zeroTask.Sand;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SandBox {
+
+    public static void main(String[] args) {
+        List<Sand> sandbox = new ArrayList<>();
+        sandbox.add(new Sand(2, "Речной"));
+        sandbox.add(new Sand(4, "Речной"));
+        sandbox.add(new Sand(2, "Карьерный"));
+        sandbox.add(new Sand(7, "Речной"));
+
+        for (Sand s : sandbox) {
+            System.out.print(s.getWeight() + " ");
+        }
+        System.out.println();
+        for (Sand s : sandbox) {
+            System.out.print(s.getName() + " ");
+        }
+        System.out.println();
+        Map<Integer, Sand> sandMap = new HashMap<>();
+        for (int i = 0; i < sandbox.size(); i++) {
+            sandMap.put(i, sandbox.get(i));
+        }
+        for (Map.Entry<Integer, Sand> m : sandMap.entrySet()) {
+            System.out.print(m.getKey() + " ");
+        }
+        System.out.println();
+        for (Map.Entry<Integer, Sand> m : sandMap.entrySet()) {
+            System.out.print(m.getValue() + " ");
+        }
+        System.out.println();
+        for (Map.Entry<Integer, Sand> m : sandMap.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
+}
